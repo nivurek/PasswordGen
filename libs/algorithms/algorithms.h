1e47c7c1445b8a7e8ce340a1c58061d696168e0c
Aidan Grigg <aidangrigg02@gmail.com>
2022-05-11 20:15:10 +1000

    Linked List structs and prototypes

--- libs/algorithms/algorithms.h
+++ libs/algorithms/algorithms.h
@@ -1,20 +1,45 @@
 #ifndef ALGORITHMS_H 
 #define ALGORITHMS_H
 
+/******************************************************************************
+ * Min Heap
+******************************************************************************/
 struct MinHNode {
     char data;
     unsigned freq;
     struct MinHNode *left, *right;
 };
-typedef struct MinHNode MinHNode_t;
+typedef struct MinHNode MHNode;
 
 struct MinHeap {
     unsigned size;
     unsigned capacity;
-    struct MinHNode_t **root;
+    MHNode **root;
 };
-typedef struct MinHeap MinHeap_t;
+typedef struct MinHeap MinHeap;
 
-MinHeap_t *createMinH(unsigned capacity);
+MinHeap *createMinH(unsigned capacity);
+
+
+/******************************************************************************
+ * Linked List
+******************************************************************************/
+struct LLNode {
+    void* data;
+    struct LLNode* next;
+};
+typedef struct LLNode LLNode;
+
+struct LinkedList {
+    unsigned int size;
+    LLNode* head;
+};
+typedef struct LinkedList LinkedList;
+
+void LL_push(LinkedList* list, void* data, size_t data_size);
+void LL_pop(LinkedList* list);
+void* LL_front(LinkedList* list);
+void LL_remove(LinkedList* list, unsigned int position);
+void LL_find(LinkedList* list, void* data);
 
 #endif
